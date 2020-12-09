2f140ef55e392ed10803ea2de30500de9284748f
lewiscutey <949124363@qq.com>
2020-12-09 20:28:26 +0800

    feat(add day20): :construction:

---
+++ Day16-20/day20.py
@@ -0,0 +1,84 @@
+"""
+异步I/O - async / await
+"""
+import asyncio
+
+
+"""指定范围的数字生成器"""
+def num_generator(m, n):
+    yield from range(m, n + 1)
+
+
+"""素数过滤器"""
+async def prime_filter(m, n):
+    primes = []
+    for i in num_generator(m, n):
+        flag = True
+        for j in range(2, int(i ** 0.5 + 1)):
+            if i % j == 0:
+                flag = False
+                break
+        if flag:
+            print('Prime =>', i)
+            primes.append(i)
+
+        await asyncio.sleep(0.001)
+    return tuple(primes)
+
+
+"""平方映射器"""
+async def square_mapper(m, n):
+    squares = []
+    for i in num_generator(m, n):
+        print('Square =>', i * i)
+        squares.append(i * i)
+
+        await asyncio.sleep(0.001)
+    return squares
+
+
+def main():
+    """主函数"""
+    loop = asyncio.get_event_loop()
+    future = asyncio.gather(prime_filter(2, 100), square_mapper(1, 100))
+    future.add_done_callback(lambda x: print(x.result()))
+    loop.run_until_complete(future)
+    loop.close()
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+import asyncio
+import re
+
+import aiohttp
+
+PATTERN = re.compile(r'\<title\>(?P<title>.*)\<\/title\>')
+
+async def fetch_page(session, url):
+    async with session.get(url, ssl=False) as resp:
+        return await resp.text()
+
+
+async def show_title(url):
+    async with aiohttp.ClientSession() as session:
+        html = await fetch_page(session, url)
+        print(PATTERN.search(html).group('title'))
+
+
+def main():
+    urls = ('https://www.python.org/',
+            'https://git-scm.com/',
+            'https://www.jd.com/',
+            'https://www.taobao.com/',
+            'https://www.douban.com/')
+    loop = asyncio.get_event_loop()
+    cos = [show_title(url) for url in urls]
+    loop.run_until_complete(asyncio.wait(cos))
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()

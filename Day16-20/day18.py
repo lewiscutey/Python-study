fec9193efbd3a928a2f08bbf0d9627f7a387d0dd
lewiscutey <949124363@qq.com>
2020-12-09 20:18:59 +0800

    feat(add day18):

--- Day16-20/day18.py
+++ Day16-20/day18.py
@@ -5,3 +5,48 @@ items1 = list(map(lambda x: x ** 2, filter(lambda x: x % 2, range(1, 10))))
 items2 = [x ** 2 for x in range(1, 10) if x % 2]
 print(items1)
 print(items2)
+
+
+"""迭代器"""
+class Fib(object):
+    
+    def __init__(self, num):
+        self.num = num
+        self.a, self.b = 0, 1
+        self.idx = 0
+   
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.idx < self.num:
+            self.a, self.b = self.b, self.a + self.b
+            self.idx += 1
+            return self.a
+        raise StopIteration()
+
+
+"""生成器"""
+def fib(num):
+    a, b = 0, 1
+    for _ in range(num):
+        a, b = b, a + b
+        yield a
+
+
+
+"""流式计算平均值"""
+def calc_avg():
+    total, counter = 0, 0
+    avg_value = None
+    while True:
+        value = yield avg_value
+        total, counter = total + value, counter + 1
+        avg_value = total / counter
+
+
+gen = calc_avg()
+next(gen)
+print(gen.send(10))
+print(gen.send(20))
+print(gen.send(30))

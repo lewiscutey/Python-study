6f1b1e56b4a77e3cd2096eba2f3f533025b608fc
lewiscutey <949124363@qq.com>
2020-12-10 22:01:09 +0800

    feat(add day21): :construction:

---
+++ Day21-30/day21.py
@@ -0,0 +1,46 @@
+'''
+linux系统学习
+'''
+
+"""基础命令"""
+
+1. 获取登录信息 - w / who / last/ lastb。
+2. 查看自己使用的Shell - ps。
+3. 查看命令的说明和位置 - whatis / which / whereis。
+4. 清除屏幕上显示的内容 - clear。
+5. 查看帮助文档 - man / info / --help / apropos。
+6. 查看系统和主机名 - uname / hostname。
+7. 时间和日期 - date / cal。
+8. 重启和关机 - reboot / shutdown。
+9. 退出登录 - exit / logout。
+10 .查看历史命令 - history。
+
+
+"""实用程序"""
+
+1. 创建/删除空目录 - mkdir / rmdir。
+2. 创建/删除文件 - touch / rm。
+3. 切换和查看当前工作目录 - cd / pwd。
+4. 查看目录内容 - ls。
+5. 查看文件内容 - cat / tac / head / tail / more / less / rev / od。
+6. 拷贝/移动文件 - cp / mv。
+7. 文件重命名 - rename。
+8. 查找文件和查找内容 - find / grep。
+9. 创建链接和查看链接 - ln / readlink。
+10. 压缩/解压缩和归档/解归档 - gzip / gunzip / xz。
+11. 归档和解归档 - tar。
+12. 将标准输入转成命令行参数 - xargs。
+13. 显示文件或目录 - basename / dirname。
+14. 其他相关工具。
+  sort - 对内容排序
+  uniq - 去掉相邻重复内容
+  tr - 替换指定内容为新内容
+  cut / paste - 剪切/黏贴内容
+  split - 拆分文件
+  file - 判断文件类型
+  wc - 统计文件行数、单词数、字节数
+  iconv - 编码转换
+
+
+
+
